5ac6356eced80d018be75baae52179284a5900b9
Angus <angus.goody@gmail.com>
2020-09-04 22:18:36 +0100

    Added Filter bar to projects page

--- projects/index.php
+++ projects/index.php
@@ -18,17 +18,46 @@ include_once include_local_file("/includes/a_config.php");
     <div class="hero-body">
       <div class="container has-text-centered">
         <h1 class="title">
-          Projects
+          My Projects
         </h1>
         <h2 class="subtitle">
           View my Projects
         </h2>
+        <br>
+        <div class="tabs is-centered is-toggle">
+          <ul>
+            <li class="is-active">
+              <a>
+                <span class="icon is-small"><i class="fas fa-th-large"></i></span>
+                <span>All</span>
+              </a>
+            </li>
+            <li>
+              <a>
+                <span class="icon is-small"><i class="fab fa-chrome"></i></span>
+                <span>Web</span>
+              </a>
+            </li>
+            <li>
+              <a>
+                <span class="icon is-small"><i class="fas fa-desktop"></i></span>
+                <span>Desktop</span>
+              </a>
+            </li>
+            <li>
+              <a>
+                <span class="icon is-small"><i class="fas fa-mobile-alt"></i></span>
+                <span>Mobile</span>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </section>
   <!-- Content -->
   <div id="wrapper" style="background-color: #1E2C2A;">
-    <div class="container section">
+    <div class="container section">    
       <!--Columns-->
       <div class="columns is-multiline">
         <? for ($i=0; $i < 10; $i++): ?>
@@ -36,9 +65,9 @@ include_once include_local_file("/includes/a_config.php");
         <div class="column is-4 is-3-fullhd">
           <div class="card">
             <div class="card-image">
-                <figure class="image is-4by3">
-                  <img src="/assets/images/project-images/default_image.svg" alt="Placeholder image">
-                </figure>
+              <figure class="image">
+                <img src="/assets/images/project-images/default_image.svg" alt="Placeholder image">
+              </figure>
             </div>
             <!--Content-->
             <div class="card-content">
